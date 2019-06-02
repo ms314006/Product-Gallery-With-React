20ff711f4ceafadd92e6f847480239434849254d
GQSM <ms314006@hotmail.com>
2019-06-02 15:40:03 +0800

    [ADD] Add 主版頁面

---
+++ src/component/Main/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Contents from '../Contents';
+import styles from './index.scss';
+
+const Main = () => (
+  <div className={styles.main_block}>
+    <Contents />
+  </div>
+);
+
+export default Main;
