26bc27d32f759c92b371f04dc3b761d5fc3e65c8
GQSM <ms314006@hotmail.com>
2019-06-02 17:08:13 +0800

    [ADD] 先建立假資料傳給 Content 顯示

--- src/component/Contents/Contents.js
+++ src/component/Contents/Contents.js
@@ -2,9 +2,26 @@ import React from 'react';
 import Content from '../Content';
 import styles from './index.scss';
 
+const falseData = [
+  {
+    type: 'border',
+    gridIndex: {
+      rowStart: '1', rowEnd: '2', columnStart: '1', columnEnd: '2',
+    },
+  },
+  {
+    type: 'fill',
+    gridIndex: {
+      rowStart: '3', rowEnd: '4', columnStart: '5', columnEnd: '7',
+    },
+  }
+];
+
 const Contents = () => (
   <div className={styles.contents_block}>
-    <Content />
+    {
+      falseData.map(datum => <Content key={datum.type} displayData={datum} />)
+    }
   </div>
 );
 
