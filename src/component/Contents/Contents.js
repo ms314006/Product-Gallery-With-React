c1731beec49a8ff653369f69f208f18ae4f23bcd
GQSM <ms314006@hotmail.com>
2019-06-03 22:59:42 +0800

    [ADD] 將所有資料都放到頁面上去

--- src/component/Contents/Contents.js
+++ src/component/Contents/Contents.js
@@ -1,32 +1,190 @@
 import React from 'react';
+import uuidv4 from 'uuid/v4';
 import Content from '../Content';
 import styles from './index.scss';
 
 const falseData = [
   {
+    id: uuidv4(),
     type: 'border',
     gridIndex: {
-      rowStart: '1', rowEnd: '2', columnStart: '1', columnEnd: '2',
+      rowStart: '4', rowEnd: '8', columnStart: '6', columnEnd: '10',
     },
+    zIndex: 0,
   },
   {
-    type: 'fill',
+    id: uuidv4(),
+    type: 'border',
     gridIndex: {
-      rowStart: '3', rowEnd: '4', columnStart: '5', columnEnd: '7',
+      rowStart: '5', rowEnd: '9', columnStart: '13', columnEnd: '17',
     },
+    zIndex: 0,
   },
   {
+    id: uuidv4(),
+    type: 'border',
+    gridIndex: {
+      rowStart: '15', rowEnd: '19', columnStart: '12', columnEnd: '16',
+    },
+    zIndex: 0,
+  },
+  {
+    id: uuidv4(),
+    type: 'border',
+    gridIndex: {
+      rowStart: '18', rowEnd: '22', columnStart: '6', columnEnd: '10',
+    },
+    zIndex: 0,
+  },
+  {
+    id: uuidv4(),
+    type: 'border',
+    gridIndex: {
+      rowStart: '28', rowEnd: '30', columnStart: '3', columnEnd: '7',
+    },
+    zIndex: 0,
+  },
+  {
+    id: uuidv4(),
+    type: 'border',
+    gridIndex: {
+      rowStart: '32', rowEnd: '38', columnStart: '2', columnEnd: '6',
+    },
+    zIndex: 0,
+  },
+  {
+    id: uuidv4(),
+    type: 'border',
+    gridIndex: {
+      rowStart: '35', rowEnd: '38', columnStart: '9', columnEnd: '17',
+    },
+    zIndex: 0,
+  },
+  {
+    id: uuidv4(),
     type: 'line',
     gridIndex: {
-      rowStart: '2', rowEnd: '4', columnStart: '1', columnEnd: '2',
+      rowStart: '12', rowEnd: '14', columnStart: '2', columnEnd: '6',
     },
+    zIndex: 20,
+  },
+  {
+    id: uuidv4(),
+    type: 'line',
+    gridIndex: {
+      rowStart: '24', rowEnd: '26', columnStart: '13', columnEnd: '17',
+    },
+    zIndex: 20,
+  },
+  {
+    id: uuidv4(),
+    type: 'line',
+    gridIndex: {
+      rowStart: '24', rowEnd: '26', columnStart: '2', columnEnd: '6',
+    },
+    zIndex: 20,
+  },
+  {
+    id: uuidv4(),
+    type: 'fill',
+    gridIndex: {
+      rowStart: '6', rowEnd: '10', columnStart: '12', columnEnd: '16',
+    },
+    zIndex: 0,
+  },
+  {
+    id: uuidv4(),
+    type: 'fill',
+    gridIndex: {
+      rowStart: '16', rowEnd: '18', columnStart: '1', columnEnd: '5',
+    },
+    zIndex: 0,
+  },
+  {
+    id: uuidv4(),
+    type: 'fill',
+    gridIndex: {
+      rowStart: '33', rowEnd: '37', columnStart: '8', columnEnd: '10',
+    },
+    zIndex: 0,
+  },
+  {
+    id: uuidv4(),
+    type: 'fill',
+    gridIndex: {
+      rowStart: '37', rowEnd: '41', columnStart: '3', columnEnd: '7',
+    },
+    zIndex: 0,
+  },
+  {
+    id: uuidv4(),
+    type: 'product',
+    gridIndex: {
+      rowStart: '5', rowEnd: '13', columnStart: '1', columnEnd: '9',
+    },
+    image: 'img01',
+    zIndex: 10,
+  },
+  {
+    id: uuidv4(),
+    type: 'product',
+    gridIndex: {
+      rowStart: '7', rowEnd: '11', columnStart: '11', columnEnd: '15',
+    },
+    image: 'img02',
+    zIndex: 10,
+  },
+  {
+    id: uuidv4(),
+    type: 'product',
+    gridIndex: {
+      rowStart: '17', rowEnd: '21', columnStart: '1', columnEnd: '9',
+    },
+    image: 'img03',
+    zIndex: 10,
+  },
+  {
+    id: uuidv4(),
+    type: 'product',
+    gridIndex: {
+      rowStart: '17', rowEnd: '25', columnStart: '13', columnEnd: '17',
+    },
+    image: 'img04',
+    zIndex: 10,
+  },
+  {
+    id: uuidv4(),
+    type: 'product',
+    gridIndex: {
+      rowStart: '25', rowEnd: '29', columnStart: '5', columnEnd: '9',
+    },
+    image: 'img05',
+    zIndex: 10,
+  },
+  {
+    id: uuidv4(),
+    type: 'product',
+    gridIndex: {
+      rowStart: '29', rowEnd: '37', columnStart: '9', columnEnd: '17',
+    },
+    image: 'img06',
+    zIndex: 10,
+  },
+  {
+    id: uuidv4(),
+    type: 'product',
+    gridIndex: {
+      rowStart: '33', rowEnd: '41', columnStart: '1', columnEnd: '5',
+    },
+    image: 'img07',
+    zIndex: 10,
   }
 ];
 
 const Contents = () => (
   <div className={styles.contents_block}>
     {
-      falseData.map(datum => <Content key={datum.type} displayData={datum} />)
+      falseData.map(datum => <Content key={datum.id} displayData={datum} />)
     }
   </div>
 );
