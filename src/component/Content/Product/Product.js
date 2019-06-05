8f52207eadc3ac63975fdaf8d651f659a5e0758a
GQSM <ms314006@hotmail.com>
2019-06-05 20:30:53 +0800

    [ADD] 加上 router 切換

--- src/component/Content/Product/Product.js
+++ src/component/Content/Product/Product.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from './index.scss';
 
 const Product = (props) => {
@@ -8,11 +9,14 @@ const Product = (props) => {
     <div
       className={styles.product_style}
       style={{
+        display: 'flex',
         backgroundImage: `url(./image/${displayData.image}.jpg)`,
         gridArea: `${gridIndex.rowStart}/${gridIndex.columnStart}/${gridIndex.rowEnd}/${gridIndex.columnEnd}`,
         zIndex: displayData.zIndex,
       }}
-    />
+    >
+      <Link to="productList" style={{ display: 'block', height: '100%', width: '100%', }} />
+    </div>
   );
 };
 
--- src/component/Contents/Contents.js
+++ src/component/Contents/Contents.js
@@ -1,16 +1,32 @@
 import React from 'react';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 import Title from '../Title';
 import Content from '../Content';
 import styles from './index.scss';
 import pageData from '../../static/pageData.js';
 
-const Contents = () => (
-  <div className={styles.contents_block}>
+const ProductList = () => (
+  <>
     <Title />
     {
       pageData.map(datum => <Content key={datum.id} displayData={datum} />)
     }
-  </div>
+  </>
+);
+
+const Test = () => (
+  <div>1111111111111</div>
+);
+
+const Contents = () => (
+  <HashRouter>
+    <div className={styles.contents_block}>
+      <Switch>
+        <Route exact path="/" component={ProductList} />
+        <Route path="/productList" component={Test} />
+      </Switch>
+    </div>
+  </HashRouter>
 );
 
 export default Contents;
